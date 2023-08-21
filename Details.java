18a19a0e711c46683d610536325e558526520a2a
Inod Bandara <87967504+inodbandara-official@users.noreply.github.com>
2023-08-21 10:43:20 +0530

    Create Details.java
    Updated the details.

---
+++ Details.java
@@ -0,0 +1,6 @@
+public class Details {
+  public static void main(String[] args) {
+    System.out.println("This file contains so many 'black' web links! Use them carefully.");
+    System.out.println("Have a nice day!!!");
+  }
+}
